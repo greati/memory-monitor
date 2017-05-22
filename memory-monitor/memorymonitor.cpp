79e73c959147cf23c10a224c6708074050e21f48
Vitor Greati <greati@ufrn.edu.br>
2017-05-22 19:03:30 +0000

    Include process table and process graph

--- memory-monitor/memorymonitor.cpp
+++ memory-monitor/memorymonitor.cpp
@@ -6,6 +6,47 @@ MemoryMonitor::MemoryMonitor(QWidget *parent) :
     ui(new Ui::MemoryMonitor)
 {
     ui->setupUi(this);
+
+    v = 0;
+
+    QObject::connect(&m_timer,SIGNAL(timeout()), this, SLOT(handleTimeout()));
+
+    m_timer.setInterval(1000);
+
+
+
+    // Main memory usage
+    ui->gen_monitor->addGraph();
+    ui->gen_monitor->graph(0)->setPen(QPen(Qt::blue));
+    ui->gen_monitor->graph(0)->setData(memX,memY);
+
+    ui->gen_monitor->xAxis->setLabel("Usage");
+    ui->gen_monitor->yAxis->setLabel("Time");
+    ui->gen_monitor->yAxis->setRange(0,100);
+    ui->gen_monitor->xAxis->setRange(0,60);
+
+    // Proc graph
+    ui->proc_graph->addGraph();
+    ui->proc_graph->graph(0)->setPen(QPen(Qt::blue));
+    ui->proc_graph->graph(0)->setData(memX,memY);
+
+    ui->proc_graph->xAxis->setLabel("Usage");
+    ui->proc_graph->yAxis->setLabel("Time");
+    ui->proc_graph->yAxis->setRange(0,100);
+    ui->proc_graph->xAxis->setRange(0,60);
+
+    m_timer.start();
+
+}
+
+void MemoryMonitor::handleTimeout(){
+    qreal x = v;//ui->gen_monitor->width() / ui->gen_monitor->xAxis->tickStep();
+    qreal y = 50;
+    memY.append(y);
+    memX.append(x);
+    ui->gen_monitor->graph(0)->setData(memX,memY);
+    ui->gen_monitor->replot();
+    v += 1;
 }
 
 MemoryMonitor::~MemoryMonitor()
--- memory-monitor/memorymonitor.h
+++ memory-monitor/memorymonitor.h
@@ -2,6 +2,7 @@
 #define MEMORYMONITOR_H
 
 #include <QMainWindow>
+#include <QtCore/QTimer>
 
 namespace Ui {
 class MemoryMonitor;
@@ -15,8 +16,15 @@ public:
     explicit MemoryMonitor(QWidget *parent = 0);
     ~MemoryMonitor();
 
+public slots:
+    void handleTimeout();
+
 private:
+    QTimer m_timer;
     Ui::MemoryMonitor *ui;
+    QVector<double> memX, memY;
+    double v;
+
 };
 
 #endif // MEMORYMONITOR_H
